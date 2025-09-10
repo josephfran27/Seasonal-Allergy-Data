31b0a50ea3fba399f53253d64df949079e203c2c
Joseph France <jafcxm@umsystem.edu>
2025-09-10 10:51:27 -0500

    Created the skeleton for the location submission section.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,75 @@ import { GiLindenLeaf } from "react-icons/gi";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState({ lat: '', lon: ' '})
+  const [pollenData, setPollenData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [locationName, setLocationName] = useState('')
+
+  const handleSubmit = () => {
+    if(location.lat && location.lon) {
+      fetchPollenData(location.lat, location.lon)
+    }
+  }
 
   return (
-    <>
-      <GiLindenLeaf />
-      <h1>Welcome to Joe's Allergy Data because his allergies suck!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="app-container">
+      <div className="header">
+        <GiLindenLeaf className="header-icon"/>
+        <h1>Pollen Data</h1>
+        <h3>Enter your location for summary of the pollen/allergens around you!</h3>
+      </div>
+
+      <div className="input-section">
+        <div className="input-grid">
+          <div className="input-group">
+            <label>Latitude</label>
+            <input
+              type="number"
+              placeholder="Input latitude"
+              step="any"
+              value={location.lat}
+            />
+            <label>Longitude</label>
+            <input
+              type="number"
+              placeholder="Input longitude"
+              step="any"
+              value={location.lon}
+            />
+          </div>
+        </div>
+
+        <div className="button-group">
+          <button 
+            onClick={handleSubmit}
+            disabled={loading || !location.lat || !location.lon}
+            className='submit-location-button'
+          >
+            {loading ? (
+              <>Fetching Pollen Data...</>
+            ) : (
+              <>
+                Get Pollen Data
+              </>
+            )}
+          </button>
+
+          <button
+            onClick={getCurrentLocation}
+            disabled={loading}
+            className='current-location-button'
+          >
+            Use my location
+          </button>
+        </div>
+
+        {/* insert stuff for after location is found */}
       </div>
       <p className="little-green-text">
         Hopefully you can learn something from this!
       </p>
-    </>
+    </div>
   )
 }
 
